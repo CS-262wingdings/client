06f0936e6ded7f716daefa57b4e21ad56fbd0b07
andrewmanq <Everest42>
2018-10-24 17:38:05 -0400

    added a passing view

---
+++ app/src/main/java/wingdings/cs262/calvin/edu/pigeonpoll/PassScreen1.java
@@ -0,0 +1,13 @@
+package wingdings.cs262.calvin.edu.pigeonpoll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PassScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_screen1);
+    }
+}
